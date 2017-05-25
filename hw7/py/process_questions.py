fa1a86d40fcfe63d6eb436be70e727a0666f5de5
cojaroge <cojaroge@ucsc.edu>
2017-05-25 14:43:24 -0700

    fixed bug for expected questionID output

--- hw7/py/process_questions.py
+++ hw7/py/process_questions.py
@@ -100,7 +100,7 @@ def read_dep(fh):
             return update_inconsistent_tags("\n".join(dep_lines)), qID
         elif re.match(r"^QuestionId:\s+(.*)$", line):
             # print(line)
-            qID = line
+            qID = line.split(': ',1)[1]
             # You would want to get the question id here and store it with the parse
             continue
         dep_lines.append(line)
@@ -179,7 +179,7 @@ def start(filename_arg):
     dep_graphs_listofEachFile = [read_dep_parses(input_file,'hw7_dataset/' + question_order[i] + '.questions.dep') for i in range(len(question_order))]
     dep_graphs = [j for i in dep_graphs_listofEachFile for j in i]
     # [print(DependencyGraph(y)) for val in dep_graphs for (x,y) in val]
-    # print(dep_graphs)
+    print(dep_graphs)
 
     #Par questions:
     #...
@@ -199,7 +199,7 @@ def start(filename_arg):
     #dep:
     #[(questionID, string_stuff), ...] 
     #to fully load into dependency graphs just load the following into variable (depending on which story):
-    # [DependencyGraph(y) for (x,y) in dep_graph_file]
+    #example_dependency_graph_list = [DependencyGraph(string_stuff) for (questionID,string_stuff) in dep_graph_file]
     #this will make a list of dependencygraphs. (you cannot store a list of dependency graphs)
     for file in question_order:
         pickler(pickles_path + pickles_dep_path + file + '.dep.pickle',[x for x in dep_graphs if file in x[0]])
