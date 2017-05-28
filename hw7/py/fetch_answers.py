5338396516c94636d5748ebe41fa4e01c8597d33
Alex <alex@clancylang.com>
2017-05-28 15:49:16 -0700

    added colors to terminal output to save our time and eyesight

--- hw7/py/fetch_answers.py
+++ hw7/py/fetch_answers.py
@@ -8,6 +8,17 @@
 #	receives dic of Q, fetches the sentence from .story or .sch file that holds the correct answer to the Q
 ##############
 
+# for coloring the terminal output 8)
+class c:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 import pickle, re
 from nltk.parse import DependencyGraph
 import fetch_sentence, cull_words, process_questions, dep_parse
@@ -77,12 +88,12 @@ def start(filename_arg):
         # print(name + ': ' + str(sgraphs))
         for qgraph in dependency_graph_list:
             if name in qgraph[0]:
-                print("Question:", dep_parse.pretty_question(qgraph[1]), "?")
+                print(c.OKGREEN + "Question:" + c.ENDC, dep_parse.pretty_question(qgraph[1]), "?")
                 answer = dep_parse.find_answer(qgraph[1], sgraphs)
                 if(answer != None):
-                    print("Answer:", answer)
+                    print(c.OKGREEN + "Answer:", answer + c.ENDC)
                 else:
-                    print("cannot answer this yet")
+                    print(c.FAIL +  "Cannot answer this yet")
                 print()
     ########## WARNING!!! ############
     #the above code will return 'None' for most... this is OK for now.
@@ -144,59 +155,19 @@ def start(filename_arg):
     # # 4.
     # # we return a list of tups [(Q1, A1), (Q2, A2), ...]
     # return QandA
-    
-# def start(bobby):
 
-#     # 0. get list of pickle files
-#     pickle_files = []
-#     with open(bobby, 'r') as bob:
-#         pickle_files = bob.read().split('\n')
-#     print(pickle_files)
-
-#     # 1.
+#     -----------THIS IS THE FORMAT WE WANT OUR ANSWER LIST TO RETURN IN------------
 #     # this is a list of lists
 #     # each element in this list is a list corresponding the each pickle file (fables-01, fables-02, etc.)
 #     # element of the inner lists is a tuple of type:
 #     # (question ID, Question, Type, Answer)
 #     # the whole thing looks like this:
-#     # all_question = 
+#     # all_questions = 
 #     # [
 #     #     [ (fables-01-1, "What is Bob?", "Story", "Dumb"), (...), ...  ],
 #     #     [ (fables-02-1, ..., ..., ...), (...), ... ],
 #     #     ...
 #     # ]
-#     all_questions = [load_pickle(f + '.pickle') for f in pickle_files]
-
-#     # 2.
-#     # now we want to read from the proper story/sch for each question and find answer sentence
-#     answer_sentences = [fetch_sentence.fetch(key, value[0].lower(), value[1].lower()) 
-#         for key, value in all_questions]
-
-#     # print(answer_sentences)
-
-#     # compile list of question/answer sentence
-#     QandA = []
-#     i = 0
-#     for key, val in all_questions.items():
-#         QandA.append((val[0], answer_sentences[i]))
-#         i += 1
-#     #print(QandA)
-
-#     # 3.
-#     # finally, we get the proper answer string for each sentence/question
-#     answers = [cull_words.cull(question, sentence) for (question, sentence) in QandA]
-
-#     QandA = []
-#     i = 0
-#     for key, val in all_questions.items():
-#         answer = ' '.join(w for w in answers[i])
-#         QandA.append(("QuestionID: " + key, "Answer: " + answer))
-#         i += 1
-#     #print(QandA)
-
-#     # 4.
-#     # we return a list of tups [(Q1, A1), (Q2, A2), ...]
-#     return QandA
 
 if __name__ == '__main__':
     start('process_stories.txt')
